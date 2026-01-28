9647c2c02c8eba6cc44c969fd6eb7b13b105bcd7
vrain1642002 <nhqviet.dth.tuyan@gmail.com>
2026-01-28 09:21:00 +0700

    fi

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -29,7 +29,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "79d3167216aa81ffb7699109b2a11619
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "bcd1f82aafaaf5e444d90ec20d594030",
+"flutter_bootstrap.js": "a7c66694ec4a2cdf7a7de1c8aabca449",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
